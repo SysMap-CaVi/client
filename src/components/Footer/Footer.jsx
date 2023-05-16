d38e39212c5964135bcce4b327483df4880ddc2a
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2023-05-16 12:47:03 -0300

    feat(footer): implements footer and responsivity

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,22 @@
+import instaIcon from "../../assets/icons/insta-icon.svg";
+import linkedinIcon from "../../assets/icons/linkedin-icon.svg";
+
 import "./Style.css";
 
 function Footer() {
   return (
     <footer className="footer-container">
-      <h1>Footer</h1>
+      <div className="footer-text">
+        <h3>Politica de privacidade | Copyright © 2023</h3>
+      </div>
+      <div className="social-icons">
+        <a href="https://www.instagram.com/ameixagoiana/" target="blank">
+          <img src={instaIcon} alt="Instagram Icon" />
+        </a>
+        <a href="https://www.linkedin.com/company/ameixagoiana/" target="blank">
+          <img src={linkedinIcon} alt="Linkedin Icon" />
+        </a>
+      </div>
     </footer>
   );
 }
